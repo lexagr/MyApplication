b12a44f89ed17f26776374f23245389a173bbb13
Aleksey <lexa.grydnistiy123@gmail.com>
2019-03-26 12:04:15 +0200

    SetMonthbarVisible implementation

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,19 +1,34 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val materialCalendarView = view?.findViewById<MaterialCalendarView>(R.id.calendarView)
+        //val materialCalendarView = view?.findViewById<MaterialCalendarView>(R.id.calendarView)
+        val materialCalendarView = inflater.inflate(R.layout.fragment_home, container, false)
         Log.d("ADebugTag", materialCalendarView.toString())
-        materialCalendarView?.topbarVisible = false
+        SetMonthbarVisible(materialCalendarView, false)
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return materialCalendarView
+    }
+
+    fun SetMonthbarVisible(view: View, visible: Boolean) {
+        val calendarView = view.findViewById<MaterialCalendarView>(R.id.calendarView)
+        val header = calendarView.findViewById<LinearLayout>(R.id.header)
+
+        if(visible) {
+            header.visibility = View.VISIBLE
+        }else{
+            header.visibility = View.INVISIBLE
+        }
     }
 }
